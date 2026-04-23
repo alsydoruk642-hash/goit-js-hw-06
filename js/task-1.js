7973b5a7bdc71fe0b4e6fe3806361c426fd2c2ce
Avelexis <alsydoruk642@gmail.com>
2026-04-23 22:32:53 +0200

    +++

--- js/task-1.js
+++ js/task-1.js
@@ -3,24 +3,6 @@ const customer = {
   balance: 24000,
   discount: 0.1,
   orders: ['Burger', 'Pizza', 'Salad'],
-  //  !!  Change code below this line
-  //   getBalance() {
-  //     return balance;
-  //   },
-  //   getDiscount() {
-  //     return discount;
-  //   },
-  //   setDiscount(value) {
-  //     discount = value;
-  //   },
-  //   getOrders() {
-  //     return orders;
-  //   },
-  //   addOrder(cost, order) {
-  //     balance -= cost - cost * discount;
-  //     orders.push(order);
-  //   },
-  //  !!   Change code above this line
 
   getBalance() {
     return this.balance;
